71f563d2e489cb4e05c593b61b1771df50628e4a
bellakhalAmir <amirbellakhal@gmail.com>
2021-03-09 20:43:37 +0100

    change into feature

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Bye there
+          some changes from feature
         </p>
         <a
           className="App-link"
